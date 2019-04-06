6e342ffd89f98358272bc9e7603cb041c26f1f74
Matheus Valandro Dorneles <matheus.dorneles@cwi.com.br>
2019-04-06 18:46:40 -0300

    fixed position elements in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="container">
         <Header />
-        <div>Content</div>
+        <div className="content">Content</div>
       </div>
     );
   }
