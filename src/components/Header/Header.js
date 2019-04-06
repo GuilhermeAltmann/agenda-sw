a8e3f63987bfea2b59c86e804559299854f3bb06
Matheus Valandro Dorneles <matheus.dorneles@cwi.com.br>
2019-04-06 18:42:54 -0300

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h3 className="header__title">SharedBizz</h3>
+      </div>
+    );
+  }
+}
+
+export default Header;
