7ae813ed8882b97021d0f3a3aee22c34fa6cff8a
Guilherme Pires <guilhermepires@Guilhermes-MacBook.local>
2019-04-07 14:28:33 -0300

    Ajustes

--- src/components/Rents/Rents.js
+++ src/components/Rents/Rents.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clinica from '../../assets/img/clinica.jpg';
 
 import './Rents.css'
+
 
 class Rents extends Component {
 
@@ -12,65 +13,112 @@ class Rents extends Component {
         return (
 
             <div className="rents__box">
+                {/* <form class="s131">
+                    <div class="inner-form">
+                        <div class="input-field first-wrap">
+                            <input id="search" type="text" placeholder="What are you looking for?" />
+                        </div>
+                        <div class="input-field second-wrap">
+                            <div class="input-select">
+                            <select data-trigger="" name="choices-single-defaul">
+                                <option placeholder="">CATEGORY</option>
+                                <option>Subject A</option>
+                                <option>Subject B</option>
+                                <option>Subject C</option>
+                            </select>
+                            </div>
+                        </div>
+                        <div class="input-field third-wrap">
+                            <button class="btn-search" type="button">SEARCH</button>
+                        </div>
+                    </div>
+                </form> */}
                 <h2 className="rents__title">Salas</h2>
                 <ul className="rents">
                     <li className="rents__item">
                         <div className="rents__item-header">
-                            <h4 className="rents__item-title">Sala Comercial</h4>
-                            <span className="rents__score yellow">
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                5.0
-                            </span>
-                        </div>
-                        <div className="rents__item-character">
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="clone" /> <b>32m²</b></div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="car" /> <b>1</b> Vaga</div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="calendar-day"/> <b>Sábados</b> liberados</div>
-                            <div className="rents__item-character-item-map"><FontAwesomeIcon className="rents__item-character-icon" icon="map-marker" /> Porto Alegre, Moinhos de vento</div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="money-bill" /> R$ 50,00 valor/hr</div>
-                        </div>
-                    </li>
-                    <li className="rents__item">
-                        <div className="rents__item-header">
-                            <h4 className="rents__item-title">Sala Comercial</h4>
-                                <span className="rents__score yellow">
-                                    <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                    <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                    <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                    <FontAwesomeIcon className="rents__item-character-icon yellow" icon="star-half-alt"/> 
-                                    3.5
-                                </span>
-                        </div>
-                        <div className="rents__item-character">
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="clone" /> <b>15m²</b></div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="car" /> <b>1</b> Vaga</div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="calendar-day"/> <b>Quintas</b> liberados</div>
-                            <div className="rents__item-character-item-map"><FontAwesomeIcon className="rents__item-character-icon" icon="map-marker" /> Porto Alegre, Cristal</div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="money-bill" /> R$ 60,00 valor/hr</div>
+                            <div className="rents__item-image-space">
+                                <div className="rents__item-image">
+                                    <img src={clinica} alt="Clinica" width="100" height="100"/>
+                                </div>
+                            </div>
+                            <div className="rents__contents">
+                                <div className="rents__item-contents-header">
+                                    <h4 className="rents__item-title">Sala Comercial</h4>
+                                    <span className="rents__score yellow">
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        5.0
+                                    </span>
+                                </div>
+                                <div className="rents__item-character">
+                                    <div className="rents__item-character-item bold"><FontAwesomeIcon className="rents__item-character-icon" icon="money-bill" /> R$ 30,00 valor/hr</div>
+                                    <div className="rents__item-character-item-area"><FontAwesomeIcon className="rents__item-character-icon" icon="clone" /> <b>20m²</b></div>
+                                    <div className="rents__item-character-item-day"><FontAwesomeIcon className="rents__item-character-icon" icon="calendar-day"/> <b>Sábado</b> liberado</div>                           
+                                    <div className="rents__item-character-item-map"><FontAwesomeIcon className="rents__item-character-icon" icon="map-marker" /> Porto Alegre, Moinhos de vento</div>
+                                    <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="car" /> <b>1</b> Vaga</div>
+                                </div>
+                            </div> 
                         </div>
                     </li>
                     <li className="rents__item">
                         <div className="rents__item-header">
-                            <h4 className="rents__item-title">Sala Comercial</h4>
-                            <span className="rents__score yellow">
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
-                                <FontAwesomeIcon className="rents__item-character-icon yellow" icon="star-half-alt"/>
-                                4.5
-                            </span>
+                            <div className="rents__item-image-space">
+                                <div className="rents__item-image">
+                                    <img src={clinica} alt="Clinica" width="100" height="100"/>
+                                </div>
+                            </div>
+                            <div className="rents__contents">
+                                <div className="rents__item-contents-header">
+                                    <h4 className="rents__item-title">Sala Comercial</h4>
+                                    <span className="rents__score yellow">
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star-half-alt"/> 
+                                        3.5
+                                    </span>
+                                </div>
+                                <div className="rents__item-character">
+                                    <div className="rents__item-character-item bold"><FontAwesomeIcon className="rents__item-character-icon" icon="money-bill" /> R$ 50,00 valor/hr</div>
+                                    <div className="rents__item-character-item-area"><FontAwesomeIcon className="rents__item-character-icon" icon="clone" /> <b>32m²</b></div>
+                                    <div className="rents__item-character-item-day"><FontAwesomeIcon className="rents__item-character-icon" icon="calendar-day"/> <b>Todas Quintas</b> liberadas</div>                           
+                                    <div className="rents__item-character-item-map"><FontAwesomeIcon className="rents__item-character-icon" icon="map-marker" /> Porto Alegre, Moinhos de vento</div>
+                                    <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="car" /> <b>Sem informação</b> Vaga</div>
+                                </div>
+                            </div>    
                         </div>
-                        <div className="rents__item-character">
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="clone" /> <b>20m²</b></div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="car" /> <b>Não informado</b></div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="calendar-day"/> <b>Terças</b> liberados</div>
-                            <div className="rents__item-character-item-map"><FontAwesomeIcon className="rents__item-character-icon" icon="map-marker" /> Porto Alegre, Floresta</div>
-                            <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="money-bill" /> R$ 30,00 valor/hr</div>
+                    </li>
+                    <li className="rents__item">
+                        <div className="rents__item-header">
+                            <div className="rents__item-image-space">
+                                <div className="rents__item-image">
+                                    <img src={clinica} alt="Clinica" width="100" height="100"/>
+                                </div>
+                            </div>
+                            <div className="rents__contents">
+                                <div className="rents__item-contents-header">
+                                    <h4 className="rents__item-title">Sala Comercial</h4>
+                                    <span className="rents__score yellow">
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star"/> 
+                                        <FontAwesomeIcon className="rents__item-character-icon" icon="star-half-alt"/> 
+                                        4.5
+                                    </span>
+                                </div>
+                                <div className="rents__item-character">
+                                    <div className="rents__item-character-item bold"><FontAwesomeIcon className="rents__item-character-icon" icon="money-bill" /> R$ 45,00 valor/hr</div>
+                                    <div className="rents__item-character-item-area"><FontAwesomeIcon className="rents__item-character-icon" icon="clone" /> <b>25m²</b></div>
+                                    <div className="rents__item-character-item-day"><FontAwesomeIcon className="rents__item-character-icon" icon="calendar-day"/> <b>Terça</b> liberado</div>                           
+                                    <div className="rents__item-character-item-map"><FontAwesomeIcon className="rents__item-character-icon" icon="map-marker" /> Porto Alegre, Moinhos de vento</div>
+                                    <div className="rents__item-character-item"><FontAwesomeIcon className="rents__item-character-icon" icon="car" /> <b>1</b> Vaga</div>
+                                </div>
+                            </div>   
                         </div>
                     </li>
                 </ul>  
