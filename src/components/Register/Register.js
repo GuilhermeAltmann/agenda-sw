d11b538f22473e42cf768db2d9dfadf089719d4b
Matheus Valandro <msvalandro@gmail.com>
2019-04-07 12:32:39 -0300

    changing routes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class Register extends Component {
   render() {
-    return <div>Register</div>;
+    return (
+      <div className="register__box">
+        <div className="register">
+          <h2>Teste</h2>
+        </div>
+      </div>
+    );
   }
 }
 
